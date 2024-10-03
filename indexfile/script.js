68cf6dce83ef0a3dd7c9a842077316d288166291
mehedihasan <mehedihasan645356@gmail.com>
2024-10-03 20:41:56 +0600

    vedio-page-using-api

--- indexfile/script.js
+++ indexfile/script.js
@@ -1,6 +1,6 @@
 //fetch , load and show Categories on html
-//cerate load categories
 
+//load categories
 const loadCategories = ()=>{
    fetch("https://openapi.programming-hero.com/api/phero-tube/categories")
    .then(res=> res.json())
@@ -8,6 +8,52 @@ const loadCategories = ()=>{
    .catch((error)=> console.error('error happend :' , error));
    
 }
+
+//loadvideos
+const loadVideos=()=>{
+    fetch("https://openapi.programming-hero.com/api/phero-tube/videos")
+    .then(res=> res.json())
+    .then(data => displayVideos(data.videos))
+    .catch((error)=> console.error('error happend :' , error));
+}
+
+//create display videos
+
+const displayVideos =(videos)=>{
+    const videosContainer =document.getElementById('videos')
+     videos.map(item =>{
+        const vCard = document.createElement('div')
+        vCard.classList='card bg-base-100 '
+        vCard.innerHTML=`
+            <figure class="w-full h-[200px]">
+                <img
+                src=${item.thumbnail}
+                alt="Thumbnail" class="w-full h-full object-cover" />
+            </figure>
+            <div class=" py-6 flex flex-row gap-4">
+                <div class="w-8 h-8 ">
+                  <img src=${item.authors[0]?.profile_picture} alt="profilePic" class="w-full h-full mx-auto rounded-full object-cover"/>
+                </div>
+                <div class="flex flex-col gap-1">
+                    <h3 class="text-base text-black font-semibold capitalize">${item.title}</h3>
+                    <div class="flex justify-start items-center gap-2">
+                    <p class="text-sm text-slate-400 font-normal">${item.authors[0]?.profile_name} </p>
+                     ${item.authors[0]?.verified === true ?'<img src="https://img.icons8.com/?size=48&id=98A4yZTt9abw&format=png" class="w-4 h-4"/>' : ' '} 
+                    </div>
+                    <span class="text-sm text-slate-400 font-normal">${item.others?.views}</span>
+                </div>
+            </div>
+
+        `
+      videosContainer.append(vCard)
+     })
+
+
+}
+
+
+
+
 
 
 // Create Display CAtegories
@@ -30,4 +76,5 @@ const displayCategories = (categories)=>{
 }
 
 
-loadCategories()
+loadCategories();
+loadVideos();
